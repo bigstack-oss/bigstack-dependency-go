866776b102c7b05052bbf60475333743354443eb
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-06-22 22:20:39 +0800

    feat(password): add a helper lib for password encryption and decryption

---
+++ pkg/password/password.go
@@ -0,0 +1,69 @@
+package password
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"io"
+)
+
+func Encrypt(plainText, secret string) (string, error) {
+	key := deriveKey(secret)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := gcm.Seal(nonce, nonce, []byte(plainText), nil)
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+func Decrypt(encryptedText, secret string) (string, error) {
+	key := deriveKey(secret)
+	cipherData, err := base64.StdEncoding.DecodeString(encryptedText)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(cipherData) < nonceSize {
+		return "", errors.New("invalid ciphertext")
+	}
+
+	nonce, cipherText := cipherData[:nonceSize], cipherData[nonceSize:]
+	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainText), nil
+}
+
+func deriveKey(secret string) []byte {
+	hash := sha256.Sum256([]byte(secret))
+	return hash[:]
+}
