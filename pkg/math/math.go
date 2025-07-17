31605cf8a508ee9746a8fdba98b9f3fd317e12d8
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-07-18 01:07:30 +0800

    feat(math): add HasMoreThanTwoDecimalPlaces() to check if a value if more than 2 decimal places

--- pkg/math/math.go
+++ pkg/math/math.go
@@ -6,3 +6,8 @@ func RoundDown(value float64, places int) float64 {
 	factor := math.Pow(10, float64(places))
 	return math.Floor(value*factor) / factor
 }
+
+func HasMoreThanTwoDecimalPlaces(value float64) bool {
+	scaled := value * 100
+	return scaled != float64(int64(scaled))
+}
