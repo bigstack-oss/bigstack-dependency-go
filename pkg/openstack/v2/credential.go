4d30c76f13c0e3fa39a373e8ce9241022601ae23
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-07-25 23:14:35 +0800

    feat(auths): add ValidToken func for openstack helper

--- pkg/openstack/v2/credential.go
+++ pkg/openstack/v2/credential.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/bigstack-oss/bigstack-dependency-go/pkg/wait"
 	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/credentials"
+	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/tokens"
 )
 
 func (h *Helper) CreateEc2Credential(userId, projectId, accessKey, secretKey string) (*credentials.Credential, error) {
@@ -21,4 +22,10 @@ func (h *Helper) CreateEc2Credential(userId, projectId, accessKey, secretKey str
 			Blob:      fmt.Sprintf("{\"access\":\"%s\",\"secret\":\"%s\"}", accessKey, secretKey),
 		},
 	).Extract()
+}
+
+func (h *Helper) ValidToken(token string) error {
+	ctx, cancel := context.WithTimeout(wait.CtxSeconds(30))
+	defer cancel()
+	return tokens.Get(ctx, h.Identity, token).Err
 }
