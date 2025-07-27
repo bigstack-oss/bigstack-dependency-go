8f49458a5ba7a4e139d4e2173679cafa546183a1
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-07-27 14:45:25 +0800

    feat(openstack): add IsDomainExists func() for openstack helper

--- pkg/openstack/v2/domains.go
+++ pkg/openstack/v2/domains.go
@@ -8,6 +8,21 @@ import (
 	"github.com/gophercloud/gophercloud/v2/openstack/identity/v3/domains"
 )
 
+func (h *Helper) IsDomainExists(name string) (bool, error) {
+	domains, err := h.ListDomains(&domains.ListOpts{Name: name})
+	if err != nil {
+		return false, err
+	}
+
+	for _, domain := range domains {
+		if domain.Name == name {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
 func (h *Helper) ListDomains(opts *domains.ListOpts) ([]domains.Domain, error) {
 	ctx, cancel := context.WithTimeout(wait.CtxSeconds(30))
 	defer cancel()
