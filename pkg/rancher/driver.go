7553f193dc0ffb825cabb87966bd3c6a642e32ad
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-08-10 23:04:12 +0800

    fix(rancher): add WaitNodeDriverStatus() in the rancher helper lib

--- pkg/rancher/driver.go
+++ pkg/rancher/driver.go
@@ -3,7 +3,19 @@ package rancher
 import (
 	"fmt"
 	"net/url"
+	"strings"
+
+	"github.com/bigstack-oss/bigstack-dependency-go/pkg/wait"
 )
+
+type NodeDriversResp struct {
+	NodeDrivers []NodeDriver `json:"data"`
+}
+
+type NodeDriver struct {
+	Name  string `json:"name"`
+	State string `json:"state"`
+}
 
 func (h *Helper) ActivateNodeDriver(name string) error {
 	u, err := url.Parse(h.Options.Url)
@@ -30,4 +42,53 @@ func (h *Helper) ActivateNodeDriver(name string) error {
 		u.String(),
 		resp.String(),
 	)
+}
+
+func (h *Helper) WaitNodeDriverStatus(name, state string, timeout int) error {
+	u, err := url.Parse(h.Options.Url)
+	if err != nil {
+		return err
+	}
+
+	u.Path = "/v3/nodedrivers"
+	for range timeout {
+		wait.Seconds(1)
+
+		listResp := &NodeDriversResp{}
+		resp, err := h.Http.R().
+			SetResult(listResp).
+			SetHeaders(GenAuthHeaders(h.Options.Token)).
+			Get(u.String())
+		if err != nil {
+			continue
+		}
+
+		if resp.IsError() {
+			continue
+		}
+
+		if h.isNodeDriverMatchedStatus(listResp.NodeDrivers, name, state) {
+			return nil
+		}
+	}
+
+	return fmt.Errorf(
+		"failed to wait node driver %s status %s",
+		name,
+		state,
+	)
+}
+
+func (h *Helper) isNodeDriverMatchedStatus(drivers []NodeDriver, name, state string) bool {
+	for _, driver := range drivers {
+		if driver.Name != name {
+			continue
+		}
+
+		if strings.EqualFold(driver.State, state) {
+			return true
+		}
+	}
+
+	return false
 }
