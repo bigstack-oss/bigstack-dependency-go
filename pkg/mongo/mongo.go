c08d174c0198eb78f95b5205d8b0f6316956d965
arashi.li <arashi.li@bigstack.co>
2025-10-22 15:05:03 +0800

    feat(mongo): extend the mongo to support bulk and file operation

--- pkg/mongo/mongo.go
+++ pkg/mongo/mongo.go
@@ -1,14 +1,18 @@
 package mongo
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"sync"
 
 	"github.com/bigstack-oss/bigstack-dependency-go/pkg/wait"
 	log "go-micro.dev/v5/logger"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/gridfs"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -36,6 +40,8 @@ type TxnClient interface {
 }
 
 type CollClient interface {
+	Aggregate(context.Context, any, ...*options.AggregateOptions) (*mongo.Cursor, error)
+	BulkWrite(context.Context, []mongo.WriteModel, ...*options.BulkWriteOptions) (*mongo.BulkWriteResult, error)
 	Find(context.Context, any, ...*options.FindOptions) (*mongo.Cursor, error)
 	FindOne(context.Context, any, ...*options.FindOneOptions) *mongo.SingleResult
 	FindOneAndUpdate(context.Context, any, any, ...*options.FindOneAndUpdateOptions) *mongo.SingleResult
@@ -48,6 +54,13 @@ type CollClient interface {
 	UpdateMany(context.Context, any, any, ...*options.UpdateOptions) (*mongo.UpdateResult, error)
 	CountDocuments(context.Context, any, ...*options.CountOptions) (int64, error)
 	Indexes() mongo.IndexView
+}
+
+type GridFSClient interface {
+	Find(filter any, opts ...*options.GridFSFindOptions) (*mongo.Cursor, error)
+	Delete(fileID any) error
+	OpenUploadStream(filename string, opts ...*options.UploadOptions) (*gridfs.UploadStream, error)
+	DownloadToStream(fileID any, stream io.Writer) (int64, error)
 }
 
 type CursorClient interface {
@@ -154,6 +167,14 @@ func (h *Helper) NewTxnCli() (TxnClient, error) {
 	}
 
 	return s, nil
+}
+
+func (h *Helper) NewGridFSCli(db string) (GridFSClient, error) {
+	b, err := gridfs.NewBucket(h.Client.Database(db))
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
 }
 
 func (h *Helper) GetQueryCursor(db, coll string, query bson.M, opts ...*options.FindOptions) (*mongo.Cursor, error) {
@@ -292,6 +313,96 @@ func (h *Helper) DeleteAll(db, coll string, filter any) error {
 	}
 
 	return nil
+}
+
+func (h *Helper) Aggregate(db, coll string, data any, opts ...*options.AggregateOptions) (*mongo.Cursor, error) {
+	c, err := h.NewCollCli(db, coll)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(wait.CtxSeconds(5))
+	defer cancel()
+	return c.Aggregate(ctx, data, opts...)
+}
+
+func (h *Helper) BulkWrite(db, coll string, models []mongo.WriteModel, opts ...*options.BulkWriteOptions) error {
+	c, err := h.NewCollCli(db, coll)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(wait.CtxSeconds(5))
+	defer cancel()
+	_, err = c.BulkWrite(ctx, models, opts...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Helper) UploadFile(db, filename string, file multipart.File, opts ...*options.UploadOptions) error {
+	c, err := h.NewGridFSCli(db)
+	if err != nil {
+		return nil
+	}
+
+	uploadStream, err := c.OpenUploadStream(filename, opts...)
+	if err != nil {
+		return nil
+	}
+	defer uploadStream.Close()
+
+	_, err = io.Copy(uploadStream, file)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func (h *Helper) FindFile(db string, filter any, opts ...*options.GridFSFindOptions) ([]gridfs.File, error) {
+	c, err := h.NewGridFSCli(db)
+	if err != nil {
+		return nil, err
+	}
+
+	cursor, err := c.Find(filter, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []gridfs.File
+	if err = cursor.All(context.Background(), &files); err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
+
+func (h *Helper) DownloadFile(db string, fileID any) (bytes.Buffer, error) {
+	c, err := h.NewGridFSCli(db)
+	if err != nil {
+		return bytes.Buffer{}, err
+	}
+
+	var buf bytes.Buffer
+	_, err = c.DownloadToStream(fileID, &buf)
+	if err != nil {
+		return bytes.Buffer{}, err
+	}
+
+	return buf, nil
+}
+
+func (h *Helper) DeleteFile(db string, fileID any) error {
+	c, err := h.NewGridFSCli(db)
+	if err != nil {
+		return err
+	}
+
+	return c.Delete(fileID)
 }
 
 func (h *Helper) GetAllCollections(db string) ([]string, error) {
